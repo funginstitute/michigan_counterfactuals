b85769965dfb8232f86ea6ec3727194578f9ab53
Gabe Fierro <gtfierro225@gmail.com>
2013-12-27 15:57:19 -0800

    fix dimensions and make a function

--- fixedpanel.m
+++ fixedpanel.m
@@ -1,11 +1,12 @@
+function [b, se] = fixedpanel()
 data = load('data');
 y = data.response; % these are the difference in cites
 x = data.predictor; % these are the year indicators. 0 until year of grant, then 1 thereafter
 x = double(x); % convert to double otherwise matlab complains
 y = double(y);
 
-n = 409; % 409 pairs of (MI, nonenforce)
-d = 38; % over 38 years
+n = 38; % 409 pairs of (MI, nonenforce)
+d = 427; % over 38 years
 % reshape to get into a format matlab can use
 Y = reshape(y, n, d);
 
@@ -19,12 +20,14 @@ for i = 1:n
 end
 % run regression
 [b,sig,E,V] = mvregress(X,Y,'algorithm','cwls');
+csvwrite('coefficients.csv',b);
 
 % compute standard error
 XX = cell2mat(X);
 S = kron(eye(n),sig);
 Vpcse = inv(XX'*XX)*XX'*S*XX*inv(XX'*XX);
-se = sqrt(diag(Vpcse))
+se = sqrt(diag(Vpcse));
+csvwrite('standarderror.csv',se);
 
 % plot
 xx = linspace(min(x),max(x));
