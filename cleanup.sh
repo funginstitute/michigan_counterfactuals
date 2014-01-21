6689d5fe35b9bd4a3238de319910135fe739f6a6
Gabe Fierro <gtfierro225@gmail.com>
2014-01-20 20:44:19 -0800

    add cleanup.sh script

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm data.csv cites_to_michigan.csv cites_to_nonenforces.csv out.csv yeardata.csv yeardata_withnulls.csv means.csv
+rm means.png all.png sum.png absolute_sum.png michigan_inventor_counts.png similar_inventor_counts.png all_cites_to_michigan.png all_cites_to_nonenforce.png
+
+rm coefficients.csv standarderror.csv coefficients_and_means.png
