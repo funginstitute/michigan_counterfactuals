00c050a94cc53110a88137e1472ce2f2949cd625
Gabe Fierro <gtfierro225@gmail.com>
2014-05-05 15:51:37 -0700

    change script

--- data2/citesbyyear.py
+++ data2/citesbyyear.py
@@ -5,7 +5,7 @@ def trim_fraction(text):
         return text[:text.rfind('.0')]
     return text
 
-cites = pd.read_csv('cites_to_treatment.tsv',delimiter='\t')
+cites = pd.read_csv('cites_to_controlused.tsv',delimiter='\t')
 #control = pd.read_csv('control.csv',header=None)
 #control[1] = control[1].astype(str).apply(trim_fraction)
 cites['cited'] = cites['cited'].astype(str).apply(trim_fraction)
@@ -13,6 +13,7 @@ cites['citing'] = cites['citing'].astype(str).apply(trim_fraction)
 #merged = pd.merge(control,cites,left_on=1,right_on='cited')
 #merged = merged[merged[0] != merged['state']]
 #cites = merged[['citing','cited','citing_year']]
+cites = cites[['citing','cited','citing_year']]
 cites = cites.drop_duplicates(cols=['cited','citing'])
 
 x = cites.copy()
@@ -32,5 +33,5 @@ for y in range(1975,2014):
 
     #import IPython
     #IPython.embed(user_ns = locals())
-#x.to_csv('citestocontrolbyyear.csv',index=False)
-x.to_csv('citestotreatmentbyyear.csv',index=False)
+x.to_csv('citestocontrolbyyear.csv',index=False)
+#x.to_csv('citestotreatmentbyyear.csv',index=False)
