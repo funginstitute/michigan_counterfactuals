20b99a37e9eb8666ad9da46407771e13f26aee76
Gabe Fierro <gtfierro225@gmail.com>
2014-05-07 03:52:56 -0700

    remove

--- data2/filtercites.py
+++ data2/filtercites.py
@@ -8,6 +8,5 @@ tdata = tdata.drop_duplicates(cols=['number'])
 merged = pd.merge(tdata, tcites, left_on='number', right_on='cited', how='right')
 merged = merged[merged['assignee_id_x'] != merged['assignee_id_y']]
 merged = merged[merged['state_x'] != merged['state_y']]
-print merged.columns
 print merged.head()
 merged[['citing','cited','citing_year','state_x']].drop_duplicates().to_csv('control_usedcites.tsv',index=False,sep='\t')
